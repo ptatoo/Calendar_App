752461ba66747abebba11b91faf0906025fee265
Alexander Song <88013429+ptatoo@users.noreply.github.com>
2026-02-03 18:27:41 -0800

    created a header thing

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,10 +1,33 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Drawer } from "expo-router/drawer";
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Drawer>
-      <Drawer.Screen name="index" options={{ title: "INDEX" }} />
-      <Drawer.Screen name="about" options={{ title: "ABOUT" }} />
-    </Drawer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="index"
+          options={{
+            headerTitle: "Calender",
+            drawerLabel: "Calendar",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="home-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="login"
+          options={{
+            headerTitle: "Login",
+            drawerLabel: "Login",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="person-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
