3921b647f3e7fbffc125596f11933ac0a5fde73e
VeneratedApotheosis <5432154321abcabcabcabcarahesdh@gmail.com>
2026-02-06 17:23:14 -0800

    some stuff

--- client/app/newLogin.tsx
+++ client/app/newLogin.tsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import { Button, View } from "react-native";
+import { useAuth } from "../hooks/useAuth";
 
 
 type ChildProps = {
-  setJwtToken: (token: string) => void;
+  onLogin: () => void;
 };
 
 type DisplayProps = {
   jwtToken : string;
 };
 
-const LoginButton = ({setJwtToken} : ChildProps) => {
+const LoginButton = ({onLogin} : ChildProps) => {
   return(
     <Button
     title = "u r amazing"
-      onPress={() => setJwtToken("gay")} 
+      onPress={() => onLogin()} 
     />
   )
 }
@@ -31,10 +32,18 @@ const DisplayJwtButton = ({jwtToken} : DisplayProps) =>{
 // 2. The Main Screen that React actually "Runs"
 export default function NewLogin() {
   const [jwtToken, setJwtToken] = useState<string>("");
+
+  
+const handleApiCall = async () => {
+  const response = await useAuth("123");
+  setJwtToken(JSON.stringify(response));
+}
+
   return (
     <View>
-      <LoginButton setJwtToken = {setJwtToken}/>
+      <LoginButton onLogin = {handleApiCall}/>
       <DisplayJwtButton jwtToken = {jwtToken}/>
     </View>
   );
 }
+
