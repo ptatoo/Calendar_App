7b8543ba2ff5626e563e0f96f001edfcdabfa6da
Alexander Song <88013429+ptatoo@users.noreply.github.com>
2026-02-09 20:29:02 -0800

    i made the button look cooler

--- client/app/newLogin.tsx
+++ client/app/newLogin.tsx
@@ -1,22 +1,49 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
+import { useAccessToken } from "../hooks/useAccessToken";
 import { useAuth } from "../hooks/useAuth";
+import { useProfiles } from "../hooks/useProfile";
 
 export default function LoginButton() {
   const { jwtToken, isLoading, error, request, promptAsync } = useAuth();
+  const profileProps = useProfiles(jwtToken?.sessionToken || null);
+  const accessTokenProps = useAccessToken(jwtToken?.sessionToken || null);
 
   return (
-    <View>
-      <Pressable onPress={() => promptAsync()} disabled={!request}>
+    <View style={styles.homepg}>
+      <Pressable
+        style={styles.button}
+        onPress={() => promptAsync()}
+        disabled={!request}
+      >
         {isLoading ? (
-          <Text> loading </Text>
+          <Text style={styles.buttonText}> loading </Text>
         ) : jwtToken ? (
-          <Text> logged in as f </Text>
+          <Text style={styles.buttonText}> logged in as f </Text>
         ) : (
-          <Text> login pls. </Text>
+          <Text style={styles.buttonText}> login pls. </Text>
         )}
       </Pressable>
       <Text>{jwtToken ? jwtToken.sessionToken : "asdf"}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  homepg: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+  },
+  button: {
+    padding: 12,
+    backgroundColor: "#4285F4",
+    borderRadius: 6,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+  },
+});
