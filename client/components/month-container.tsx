71395e664b7fda7f6c68cdb83ede4b1888b40426
Alexander Song <88013429+ptatoo@users.noreply.github.com>
2026-02-13 19:31:16 -0800

    pushing

--- client/components/month-container.tsx
+++ client/components/month-container.tsx
@@ -5,7 +5,6 @@ import { View } from "react-native";
 export default function MonthContainer() {
   const { days: today, refetch: refetchToday } = useDate(0, 2);
   const [firstDay, setFirstDay] = useState(new Date());
-  const [lastDay, setLastDay] = useState(new Date());
   const { days: month, refetch: refetchMonth } = useDate(0, 0);
 
   useEffect(() => {
@@ -13,15 +12,14 @@ export default function MonthContainer() {
       setFirstDay(
         new Date(today[0].date.getFullYear(), today[0].date.getMonth(), 1),
       );
-      setLastDay(
-        new Date(today[0].date.getFullYear(), today[0].date.getMonth() + 1, 0),
+      const lstDay = new Date(
+        today[0].date.getFullYear(),
+        today[0].date.getMonth() + 1,
+        0,
       );
-    }
-    if (today && today.length != 0 && lastDay) {
-      console.log("here");
       refetchMonth(
         -1 * today[0].date.getDate() + 1,
-        lastDay.getDay() - today[0].date.getDate(),
+        lstDay.getDate() - today[0].date.getDate() + 2,
       );
     }
   }, [today]);
